fa7915b1e178cbb0f83c0d35b8d8e8e676a5e993
aleclarson <alec.stanford.larson@gmail.com>
2017-03-18 15:20:54 -0400

    Add `requireNativeComponent` stub

---
+++ src/modules/requireNativeComponent.js
@@ -0,0 +1,7 @@
+/**
+ * @providesModule requireNativeComponent
+ */
+
+module.exports = function requireNativeComponent() {
+  console.warn('requireNativeComponent(...): Not supported on this platform.');
+};
